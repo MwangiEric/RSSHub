9eb575ffc9a4336ca1ede2ba32a0cb1878a72c48
MwangiEric <54898463+MwangiEric@users.noreply.github.com>
2026-02-21 21:25:04 +0300

    Create radar.ts

---
+++ lib/routes/kenyatronics/radar.ts
@@ -0,0 +1,14 @@
+export const radar = [
+    {
+        source: ['kenyatronics.com/'],
+        target: '/kenyatronics',
+    },
+    {
+        source: ['kenyatronics.com/search'],
+        target: '/kenyatronics/search/:keyword',
+    },
+    {
+        source: ['kenyatronics.com/view-product/:productId'],
+        target: '/kenyatronics/product/:productId',
+    },
+];
